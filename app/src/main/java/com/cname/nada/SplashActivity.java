1a6a0dfd6efbdc90fbe104b3e6fc976d28e900cc
seongmin <hsmin040326@gmail.com>
2021-07-27 14:36:02 +0900

    InternetConnection Checking add

--- app/src/main/java/com/cname/nada/SplashActivity.java
+++ app/src/main/java/com/cname/nada/SplashActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import static com.cname.nada.functions.CheckInternetState.isConnected;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        isConnected(getApplicationContext());
+
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
         finish();
+
     }
 }
