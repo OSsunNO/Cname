ac8d0b7acc69c28ff29b9638baedcc314de22755
gom <gom12602@gmail.com>
2021-07-27 12:36:42 +0900

    Revert "return button add"
    This reverts commit 804262f5

--- app/src/main/java/com/cname/nada/List_search.java
+++ app/src/main/java/com/cname/nada/List_search.java
@@ -1,27 +1,15 @@
 package com.cname.nada;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class List_search extends AppCompatActivity {
-
-    TextView btnReturn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_search_page);
-
-        btnReturn = (TextView) findViewById(R.id.returnBtn);
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
     }
 }
--- app/src/main/java/com/cname/nada/List_settings.java
+++ app/src/main/java/com/cname/nada/List_settings.java
@@ -1,27 +1,15 @@
 package com.cname.nada;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class List_settings extends AppCompatActivity {
 
-    TextView btnReturn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.send_page);
-
-        btnReturn = (TextView) findViewById(R.id.returnBtn);
-        btnReturn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        setContentView(R.layout.list_settings_page);
     }
 }
