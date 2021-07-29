9aacc33db37e020f2a6edd0bd797e7dc3859e8ea
seongmin <hsmin040326@gmail.com>
2021-07-30 01:03:41 +0900

    카드 돌아감

--- app/src/main/java/com/cname/nada/Frag1.java
+++ app/src/main/java/com/cname/nada/Frag1.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +14,10 @@ import androidx.fragment.app.Fragment;
 
 public class Frag1 extends Fragment {
     private View view;
+    private TextView nameCardIntroduction;
+    private ImageView editBtn;
+    private boolean showingBack;
+    private FrameLayout nameCardView;
 
 
     @Nullable
@@ -19,6 +26,84 @@ public class Frag1 extends Fragment {
     {
         view = inflater.inflate(R.layout.frag1,container,false);
 
+        nameCardIntroduction = (TextView) view.findViewById(R.id.nameCardIntroduction);
+        editBtn = (ImageView) view.findViewById(R.id.EditBtn);
+        nameCardView = (FrameLayout) view.findViewById(R.id.container);
+
+        showingBack = false;
+
+        if(savedInstanceState == null) {
+            getFragmentManager().beginTransaction().add(R.id.container, new CardFrontFragment()).commit();
+        }
+
+        nameCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipCard();
+                if (showingBack == true) {
+                    showingBack = false;
+                } else {
+                    showingBack = true;
+                }
+            }
+        });
+
         return view;
+    }
+
+    private void flipCard() {
+        if(showingBack) {
+            getFragmentManager().beginTransaction()
+                    .setCustomAnimations(
+                            R.animator.card_flip_right_in,
+                            R.animator.card_flip_right_out,
+                            R.animator.card_flip_left_in,
+                            R.animator.card_flip_left_out)
+                    .replace(R.id.container, new CardFrontFragment()).commit();
+            return;
+        }
+
+        // Flip to the back.
+
+        // Create and commit a new fragment transaction that adds the fragment for
+        // the back of the card, uses custom animations, and is part of the fragment
+        // manager's back stack.
+
+        getFragmentManager().beginTransaction()
+                // Replace the default fragment animations with animator resources
+                // representing rotations when switching to the back of the card, as
+                // well as animator resources representing rotations when flipping
+                // back to the front (e.g. when the system Back button is pressed).
+                .setCustomAnimations(
+                        R.animator.card_flip_right_in,
+                        R.animator.card_flip_right_out,
+                        R.animator.card_flip_left_in,
+                        R.animator.card_flip_left_out)
+                // Replace any fragments currently in the container view with a
+                // fragment representing the next page (indicated by the
+                // just-incremented currentPage variable).
+                .replace(R.id.container, new CardBackFragment())
+
+                // Add this transaction to the back stack, allowing users to press
+                // Back to get to the front of the card.
+
+                // Commit the transaction.
+                .commit();
+    }
+
+    // A fragment representing the front of the card.
+    public static class CardFrontFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_front, container, false);
+        }
+    }
+
+    // A fragment representing the back of the card.
+    public static class CardBackFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_back, container, false);
+        }
     }
 }
