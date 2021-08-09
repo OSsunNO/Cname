22c7b80bf85272ed4028d87365a7253158eaafe5
seongmin <hsmin040326@gmail.com>
2021-08-09 17:12:35 +0900

    로그인하다만거2

--- app/src/androidTest/java/com/cname/nada/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cname/nada/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.cname", appContext.getPackageName());
+        assertEquals("com.cname.nada", appContext.getPackageName());
     }
 }
