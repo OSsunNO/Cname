cba24d11c04b66b352058ca73783a58cd886dbf0
seongmini <hsmin040326@gmail.com>
2021-11-15 15:33:34 +0900

    리사이클러뷰 만들기 성공. db에서 정보만 불러와서 붙이면 됨.

--- app/src/main/java/com/cname/nada/Frag2.java
+++ app/src/main/java/com/cname/nada/Frag2.java
@@ -14,6 +14,10 @@ import android.widget.PopupMenu;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Frag2 extends Fragment {
     private View view;
@@ -25,6 +29,23 @@ public class Frag2 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.frag2,container,false);
+
+        //리사이클러뷰에 표시할 데이터 리스트 생성.
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        for (int i=0; i<100; i++) {
+            ArrayList<String> innerList = new ArrayList<>();
+            innerList.add(String.format("name %d", i));
+            innerList.add(String.format("position %d", i));
+            list.add(innerList);
+        }
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = view.findViewById(R.id.recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(list);
+        recyclerView.setAdapter(adapter);
 
         searchBtn = (ImageView) view.findViewById(R.id.SearchBtn);
         sendBtn = (ImageView) view.findViewById(R.id.SendBtn);
