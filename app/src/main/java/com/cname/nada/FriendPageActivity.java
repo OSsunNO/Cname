ade86061029c283f62db2701cfd0b805352f9fbf
gom <gom12602@gmail.com>
2021-11-23 21:01:51 +0900

    삭제

--- app/src/main/java/com/cname/nada/FriendPageActivity.java
+++ app/src/main/java/com/cname/nada/FriendPageActivity.java
@@ -1,15 +1,10 @@
 package com.cname.nada;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 
 public class FriendPageActivity extends AppCompatActivity {
 
@@ -29,79 +24,6 @@ public class FriendPageActivity extends AppCompatActivity {
 
         showingBack = false;
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction().add(R.id.container2, cardFrontFragment).commit();
-            // R.id.container, new FriendPageActivity.CardFrontFragment()
-        }
 
-        nameCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                flipCard();
-                if (showingBack == true) {
-                    showingBack = false;
-                } else {
-                    showingBack = true;
-                }
-            }
-        });
-    }
-
-    private void flipCard() {
-        if(showingBack) {
-            getFragmentManager().beginTransaction()
-                    .setCustomAnimations(
-                            R.animator.card_flip_right_in,
-                            R.animator.card_flip_right_out,
-                            R.animator.card_flip_left_in,
-                            R.animator.card_flip_left_out)
-                    .replace(R.id.container2, cardFrontFragment).commit();
-            return;
-        }
-
-        // Flip to the back.
-
-        // Create and commit a new fragment transaction that adds the fragment for
-        // the back of the card, uses custom animations, and is part of the fragment
-        // manager's back stack.
-
-        getFragmentManager().beginTransaction()
-                // Replace the default fragment animations with animator resources
-                // representing rotations when switching to the back of the card, as
-                // well as animator resources representing rotations when flipping
-                // back to the front (e.g. when the system Back button is pressed).
-                .setCustomAnimations(
-                        R.animator.card_flip_right_in,
-                        R.animator.card_flip_right_out,
-                        R.animator.card_flip_left_in,
-                        R.animator.card_flip_left_out)
-                // Replace any fragments currently in the container view with a
-                // fragment representing the next page (indicated by the
-                // just-incremented currentPage variable).
-                .replace(R.id.container2, cardBackFragment)
-
-                // Add this transaction to the back stack, allowing users to press
-                // Back to get to the front of the card.
-
-                // Commit the transaction.
-                .commit();
-    }
-
-    public class CardFlipActivity extends FragmentActivity {
-        // A fragment representing the front of the card.
-        public class CardFrontFragment extends Fragment {
-            @Override
-            public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-                return inflater.inflate(R.layout.fragment_card_front, container, false);
-            }
-        }
-
-        // A fragment representing the back of the card.
-        public class CardBackFragment extends Fragment {
-            @Override
-            public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-                return inflater.inflate(R.layout.fragment_card_back, container, false);
-            }
-        }
     }
 }
